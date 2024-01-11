d370985dd6072129f1e33bada40f5716e490f1ca
Mingo07_dev <sajeva.marco01@gmail.com>
2024-01-11 09:41:41 +0100

    add static data in mqa demo to avoid improper use

--- ngx-admin-master/src/app/pages/mqa/mqa.component.ts
+++ ngx-admin-master/src/app/pages/mqa/mqa.component.ts
@@ -17,7 +17,7 @@ export class MqaComponent implements OnInit {
   }
 
   async getCatalogue(id : String) : Promise<any>{
-    let response = await this.mqaService.getCatalogue(id);
+    let response = await this.mqaService.getCatalogue("659e6db2aedff86da56c40c8");
     document.getElementById("response").innerHTML = JSON.stringify(response, null, "\t");
   }
 
@@ -25,13 +25,13 @@ export class MqaComponent implements OnInit {
     let json =  {
       "parameters": filters,
     }
-    let response = await this.mqaService.getFiltered(id,json);
+    let response = await this.mqaService.getFiltered("659e6db2aedff86da56c40c8",json);
     document.getElementById("response").innerHTML = JSON.stringify(response, null, "\t");
   }
 
   async submitAnalisysJSON(id : String, url : String, xml : String) : Promise<any>{
     //xml will be added to the body of the request from a local json file because it is too long to be added here
-    let response = await this.mqaService.submitAnalisysJSON(id,url);
+    let response = await this.mqaService.submitAnalisysJSON("659e6db2aedff86da56c40c8",url);
     document.getElementById("response").innerHTML = JSON.stringify(response, null, "\t");
   }
 
